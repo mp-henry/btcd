f0baebe64bba8d9160e650e4204d9127d6353faf
Oliver Gugger <gugger@gmail.com>
2023-12-19 14:12:45 +0100

    chaincfg: add own module, use v2

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -12,8 +12,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcd/chainhash/v2"
+	"github.com/btcsuite/btcd/wire/v2"
 )
 
 // These variables are the chain proof-of-work limit parameters for each default
